2b012f403a7b808e6fbfdd00d64c44722ffca3fe
worktians <worktians@126.com>
2018-09-14 00:53:49 +0800

    完善过滤类 By tianwenchao

--- FileDataFactory/src/com/tians/datachace/DataFactory.java
+++ FileDataFactory/src/com/tians/datachace/DataFactory.java
@@ -1,32 +1,36 @@
 package com.tians.datachace;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class DataFactory {
-
-	public static void main(String [] aa){
-		try {
-			
-		
-			System.out.println("【startTime】:"+new Date().getTime());	
-			
-			getLogInfosByCon("", null, null, null, null, null, null);
-			
-			System.out.println("【endTime】"+new Date().getTime());
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-	}
 	
+	/** 以下变量保存上一次查询条件   */
+	/** 标识日志文件  */
+	private static String logFileID = "111";
+	/** 查询日志开始时间  */
+	private static String startTime = "";
+	/** 查询日志结束时间  */
+	private static String endTime = "";
+	/** 查询日志进程号  */
+	private static String prossNo = "";
+	/** 日志级别  */
+	private static String level = "";
+	/** 前缀标识  */
+	private static String pre = "";
+	/** 内容关键字  */
+	private static String context = "";
+	
+	public SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss SSS");
+
+	public DataFactory(){}
+	
+
 	/**
-	 * 根据条件获取json数据
+	 * 根据条件获取json数据 默认符合条件的数据最多返回一万行
 	 * @author tianwenchao 2018-09-12
 	 * @param logFileID 日志文件ID标识唯一
 	 * @param startTime 过滤开始时间
@@ -37,8 +41,16 @@ public class DataFactory {
 	 * @param context 内容关键字 模糊匹配
 	 * @return JSONObject  
 	 */
-	public static JSONObject getLogInfosByCon(String logFileID,String startTime,String endTime,String prossNo,String level,String pre,String context){
+	public JSONObject getLogInfosByCon(String aLogFileID,String aStartTime,String aEndTime,String aProssNo,String aLevel,String aPre,String aContext){
 		
+		logFileID = aLogFileID;
+		startTime = aStartTime;
+		endTime = aEndTime;
+		prossNo = aProssNo;
+		level = aLevel;
+		pre = aPre;
+		context = aContext;
+				
 		JSONObject fileJson = new JSONObject();
 		List<JSONObject> jsonList = new ArrayList<JSONObject>();
 		for(int i=0;i<100000;i++){
@@ -51,9 +63,81 @@ public class DataFactory {
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
+		
+		dataFilter(jsonList, 10000);
+		
 		System.out.println("jsonList.size:"+jsonList.size());
 		return fileJson;
 	}
+	
+	/***
+	 * 根据原始数据集合过滤所需日志
+	 * @param sourceList 原始数据集合（从后台获取到的）
+	 * @param maxLimit 最大返回行数限制
+	 * @return
+	 */
+	private List<JSONObject> dataFilter(List<JSONObject> sourceList,int maxLimit){
+		List<JSONObject> returnList = new ArrayList<JSONObject>(maxLimit);
+		if(null == sourceList || 0== sourceList.size()){
+			return returnList;
+		}
+				
+		int jsonNum = 0;
+		//所有过滤均采用反向判断，即：有一项不符合条件就跳出循环，寻找下一条
+		for(JSONObject tempJson : sourceList){
+			
+			try {
+				if(null!=startTime && !"".equals(startTime)){				
+					if(sdf.parse(tempJson.getString("time")).before(sdf.parse(startTime))){
+						break;
+					}							
+				}
+				if(null!=endTime && !"".equals(endTime)){				
+					if(sdf.parse(tempJson.getString("time")).after(sdf.parse(endTime))){
+						break;
+					}					
+				}
+				if(null!=prossNo && !"".equals(prossNo)){
+					
+					if(!prossNo.equals(tempJson.getString("prossNo"))){
+						break;
+					}							
+				}
+				if(null!=pre && !"".equals(pre)){
+					
+					if(!pre.equals(tempJson.getString("pre"))){
+						break;
+					}							
+				}
+				if(null!=level && !"".equals(level)){
+					
+					if(!level.equals(tempJson.getString("level"))){
+						break;
+					}							
+				}
+				if(null!=context && !"".equals(context)){
+					
+					if(!tempJson.getString("context").matches(context)){
+						break;
+					}							
+				}
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+				break;
+			}	
+			
+			returnList.add(tempJson);
+			jsonNum++;
+			if(jsonNum==maxLimit){
+				return returnList;
+			}
+		}
+		
+		return returnList;
+	}
+	
+	
 	
 	/**
 	 * 生成json对象
